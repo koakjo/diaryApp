fe8c6ea14c6a7a3a5dba941e900590dc68ad72ea
koakjo <kajitakemotohiko2@gmail.com>
2021-05-11 02:05:41 +0900

    日記帳アプリコミット

---
+++ diaryApp/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿
+namespace diaryApp
+{
+    partial class diaryAppForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(diaryAppForm));
+            this.mainTextBox = new System.Windows.Forms.TextBox();
+            this.dateLabel = new System.Windows.Forms.Label();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.commitButton = new System.Windows.Forms.Button();
+            this.displayButton = new System.Windows.Forms.Button();
+            this.titleTextBox = new System.Windows.Forms.TextBox();
+            this.titleName = new System.Windows.Forms.Label();
+            this.goodTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.badTextBox = new System.Windows.Forms.TextBox();
+            this.mindsetTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // mainTextBox
+            // 
+            this.mainTextBox.Font = new System.Drawing.Font("Meiryo UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.mainTextBox.Location = new System.Drawing.Point(54, 918);
+            this.mainTextBox.Multiline = true;
+            this.mainTextBox.Name = "mainTextBox";
+            this.mainTextBox.Size = new System.Drawing.Size(1328, 513);
+            this.mainTextBox.TabIndex = 5;
+            // 
+            // dateLabel
+            // 
+            this.dateLabel.AutoSize = true;
+            this.dateLabel.Enabled = false;
+            this.dateLabel.Font = new System.Drawing.Font("Meiryo UI", 19.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.dateLabel.Location = new System.Drawing.Point(12, 25);
+            this.dateLabel.Name = "dateLabel";
+            this.dateLabel.Size = new System.Drawing.Size(160, 67);
+            this.dateLabel.TabIndex = 0;
+            this.dateLabel.Text = "Date";
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.Location = new System.Drawing.Point(54, 1490);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(306, 38);
+            this.dateTimePicker.TabIndex = 6;
+            // 
+            // commitButton
+            // 
+            this.commitButton.Location = new System.Drawing.Point(1145, 1462);
+            this.commitButton.Name = "commitButton";
+            this.commitButton.Size = new System.Drawing.Size(237, 99);
+            this.commitButton.TabIndex = 8;
+            this.commitButton.Text = "Commit";
+            this.commitButton.UseVisualStyleBackColor = true;
+            this.commitButton.Click += new System.EventHandler(this.commitButton_Click);
+            // 
+            // displayButton
+            // 
+            this.displayButton.Location = new System.Drawing.Point(390, 1462);
+            this.displayButton.Name = "displayButton";
+            this.displayButton.Size = new System.Drawing.Size(328, 99);
+            this.displayButton.TabIndex = 7;
+            this.displayButton.Text = "Display";
+            this.displayButton.UseVisualStyleBackColor = true;
+            this.displayButton.Click += new System.EventHandler(this.displayButton_Click);
+            // 
+            // titleTextBox
+            // 
+            this.titleTextBox.Font = new System.Drawing.Font("Meiryo UI", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.titleTextBox.Location = new System.Drawing.Point(234, 283);
+            this.titleTextBox.Name = "titleTextBox";
+            this.titleTextBox.Size = new System.Drawing.Size(1148, 54);
+            this.titleTextBox.TabIndex = 2;
+            // 
+            // titleName
+            // 
+            this.titleName.AutoSize = true;
+            this.titleName.Enabled = false;
+            this.titleName.Font = new System.Drawing.Font("Meiryo UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.titleName.Location = new System.Drawing.Point(54, 274);
+            this.titleName.Name = "titleName";
+            this.titleName.Size = new System.Drawing.Size(128, 61);
+            this.titleName.TabIndex = 0;
+            this.titleName.Text = "Title";
+            // 
+            // goodTextBox
+            // 
+            this.goodTextBox.Font = new System.Drawing.Font("Meiryo UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.goodTextBox.Location = new System.Drawing.Point(234, 397);
+            this.goodTextBox.Multiline = true;
+            this.goodTextBox.Name = "goodTextBox";
+            this.goodTextBox.Size = new System.Drawing.Size(1148, 165);
+            this.goodTextBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Enabled = false;
+            this.label2.Font = new System.Drawing.Font("Meiryo UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(40, 451);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(149, 61);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Good";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Enabled = false;
+            this.label3.Font = new System.Drawing.Font("Meiryo UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(58, 658);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 61);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Bad";
+            // 
+            // badTextBox
+            // 
+            this.badTextBox.Font = new System.Drawing.Font("Meiryo UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.badTextBox.Location = new System.Drawing.Point(234, 609);
+            this.badTextBox.Multiline = true;
+            this.badTextBox.Name = "badTextBox";
+            this.badTextBox.Size = new System.Drawing.Size(1148, 176);
+            this.badTextBox.TabIndex = 4;
+            // 
+            // mindsetTextBox
+            // 
+            this.mindsetTextBox.BackColor = System.Drawing.SystemColors.Info;
+            this.mindsetTextBox.Font = new System.Drawing.Font("Meiryo UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.mindsetTextBox.Location = new System.Drawing.Point(334, 126);
+            this.mindsetTextBox.Name = "mindsetTextBox";
+            this.mindsetTextBox.Size = new System.Drawing.Size(1048, 68);
+            this.mindsetTextBox.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Enabled = false;
+            this.label4.Font = new System.Drawing.Font("Meiryo UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(54, 838);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(258, 61);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "本日の雑記";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Enabled = false;
+            this.label5.Font = new System.Drawing.Font("Meiryo UI", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(54, 141);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(227, 47);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "今月の気構え";
+            // 
+            // diaryAppForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(1414, 1587);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.mindsetTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.badTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.goodTextBox);
+            this.Controls.Add(this.titleName);
+            this.Controls.Add(this.titleTextBox);
+            this.Controls.Add(this.displayButton);
+            this.Controls.Add(this.commitButton);
+            this.Controls.Add(this.dateTimePicker);
+            this.Controls.Add(this.dateLabel);
+            this.Controls.Add(this.mainTextBox);
+            this.Font = new System.Drawing.Font("Meiryo UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "diaryAppForm";
+            this.Text = "自省日記";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox mainTextBox;
+        private System.Windows.Forms.Label dateLabel;
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
+        private System.Windows.Forms.Button commitButton;
+        private System.Windows.Forms.Button displayButton;
+        private System.Windows.Forms.TextBox titleTextBox;
+        private System.Windows.Forms.Label titleName;
+        private System.Windows.Forms.TextBox goodTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox badTextBox;
+        private System.Windows.Forms.TextBox mindsetTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ diaryApp/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.VisualBasic.FileIO;
+
+
+namespace diaryApp
+{ 
+
+    public partial class diaryAppForm : Form
+    {
+
+        public static String THIS_PROGRAM = "自省日記";
+
+        //本日の日付
+        DateTime dateTime = DateTime.Now;
+
+        //これらをもとに動く
+        public String rootFolderPath;
+        public String monthFolderPath;
+        public String monthKIGAMAEfilePath;
+        public String dayFilePath;
+
+        // 新規フラグ
+        public Boolean newDataFlag;
+
+        public diaryAppForm()
+        {
+            InitializeComponent();
+
+
+            //新規フラグの初期化
+            newDataFlag = false;
+
+            //フォルダパスの設定
+            setFolderPath(dateTime);
+
+            //読み込み
+             displayFiles();
+
+            //日付の設定
+            dateLabel.Text = "本日：" + dateTime.ToString("yyyy年MM月dd日");
+
+        }
+
+        //コミットボタンクリック処理
+        private void commitButton_Click(object sender, EventArgs e)
+        {
+            saveFiles();
+        }
+
+
+        //ディスプレイボタンクリック処理
+        private void displayButton_Click(object sender, EventArgs e)
+        {
+            DateTime selectDateTime = dateTimePicker.Value.Date;
+
+            cleanText();
+
+            setFolderPath(selectDateTime);
+
+            displayFiles();
+
+
+        }
+
+
+
+
+
+
+
+        //指定されたフォルダやパスをもとに内容を表示する
+        //なければ新規フラグを立てる
+        //画面終了時に新規フラグがONの場合は保存して終了するようにメッセージを出す
+        private void displayFiles()
+        {
+            try
+            {
+                // 全体フォルダの存在チェック、なければ作る
+                if (Directory.Exists(rootFolderPath)==false) { Directory.CreateDirectory(rootFolderPath); }
+
+                // 該当月フォルダの存在チェック、なければ作る
+                if (Directory.Exists(monthFolderPath)==false) { Directory.CreateDirectory(monthFolderPath); }
+
+                // 気構えファイルの存在チェック、なければ作る
+                if (File.Exists(monthKIGAMAEfilePath)==false) { File.CreateText(monthKIGAMAEfilePath); }
+
+                // 該当日ファイルの存在チェック、なければ作る
+                if (File.Exists(dayFilePath) == false) { File.CreateText(dayFilePath); }
+
+
+                //気構えファイルを読み込み、気構えフィールドに展開
+                var kigamaeParcer = new TextFieldParser(monthKIGAMAEfilePath);
+                kigamaeParcer.Delimiters = new string[] { "," };
+
+                //読み込んだCSVに中身がなければ新規判定とし、関数処理を終了
+                if (kigamaeParcer.EndOfData) { newDataFlag = true; return; }
+
+                mindsetTextBox.Text = kigamaeParcer.ReadLine();
+                if (mindsetTextBox.Text == "") { mindsetTextBox.Text = "入力してください"; }
+
+                //読み込み
+                var contentParcer = new TextFieldParser(dayFilePath);
+                contentParcer.Delimiters = new string[] { "," };
+
+
+                //読み込んだCSVに中身がなければ新規判定とし、関数処理を終了
+                if (contentParcer.EndOfData) { newDataFlag = true; return; }
+
+                //第一フィールドはタイトル
+                //第二フィールドは良かった点
+                //第三フィールドは悪かった点
+                //第四フィールドは雑記
+                var fields = contentParcer.ReadFields();
+
+                titleTextBox.Text = fields[0];
+                goodTextBox.Text = fields[1];
+                badTextBox.Text = fields[2];
+                mainTextBox.Text = fields[3];
+
+            } catch (Exception e)
+            {
+                Console.WriteLine(e.Message.ToString());
+                //握りつぶす
+
+                MessageBox.Show(e.Message.ToString());
+            }
+
+        }
+
+        //指定されているフォルダやパスをもとに内容を保存する
+        //新規フラグがfalseのときは、確認を行うようにする（モーダルでよい）
+        private void saveFiles()
+        {
+            if (newDataFlag == false) { 
+                var result = MessageBox.Show("既存ファイルを上書します。" + "\r\n" + "よろしいですか？", THIS_PROGRAM,
+                                 MessageBoxButtons.YesNo, MessageBoxIcon.Error); 
+
+                //イエス以外は処理中断
+                if (result != DialogResult.Yes)
+                {
+                    return;
+                }
+
+            }
+
+            //気構えフィールド更新
+            File.WriteAllText(monthKIGAMAEfilePath, mindsetTextBox.Text);
+
+            //コンテンツを文字列へ
+            StringBuilder contents = new StringBuilder();
+
+            contents.Append("\"" + titleTextBox.Text + "\"" + ",");
+            contents.Append("\"" + goodTextBox.Text + "\"" + ",");
+            contents.Append("\"" + badTextBox.Text + "\"" + ",");
+            contents.Append("\"" + mainTextBox.Text + "\"");
+
+            String strings = contents.ToString();
+
+            //出力
+            File.WriteAllText(dayFilePath, strings);
+
+
+
+        }
+
+        //CSVパーサー
+        //引数に渡すものによって読み込みと書き込みを分ける
+
+
+        //セットフォルダ
+        //渡されたDateTime型によって日記帳表示に使用するフォルダパスを更新する
+        private void setFolderPath(DateTime givenDateTime)
+        {
+            //フォルダパス、ファイルパスの設定
+            rootFolderPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + @"\diaryroot";
+            monthFolderPath = rootFolderPath + @"\" + givenDateTime.Year + givenDateTime.Month;
+            monthKIGAMAEfilePath = monthFolderPath + @"\" + "KIGAMAE.csv";
+            dayFilePath = monthFolderPath + @"\" + givenDateTime.Day + ".csv";
+        }
+
+        //クリーンテキスト
+        private void cleanText()
+        {
+            mindsetTextBox.Text = "";
+            titleTextBox.Text = "";
+            goodTextBox.Text = "";
+            badTextBox.Text = "";
+            mainTextBox.Text = "";
+        }
+    }
+}
